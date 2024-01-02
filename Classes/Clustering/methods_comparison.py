d9bf1f360854ac23082a033fa70ca1d5b4c725b7
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-01-02 13:31:27 -0300

    finish clustering

--- Classes/Clustering/methods_comparison.py
+++ Classes/Clustering/methods_comparison.py
@@ -2,6 +2,7 @@ import plotly.express as px
 from sklearn import datasets
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
 
 x_random, y_random = datasets.make_moons(n_samples=1500, noise=0.09)
 graph = px.scatter(x=x_random[:, 0], y=x_random[:, 1])
@@ -12,4 +13,13 @@ labels = kmeans_labels = kmeans.fit_predict(x_random)
 kmeans_graph = px.scatter(x=x_random[:, 0], y=x_random[:, 1], color=labels)
 kmeans_graph.show()
 
-hc = AgglomerativeClustering(n_clusters=2, affinity='euclidean')
+hc = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+labels = hc.fit_predict(x_random)
+
+hc_graph = px.scatter(x=x_random[:, 0], y=x_random[:, 1], color=labels)
+hc_graph.show()
+
+dbscan = DBSCAN(eps=0.1)
+labels = dbscan.fit_predict(x=x_random)
+dbscan_graph = px.scatter(x=x_random[:, 0], y=x_random[:, 1], color=labels)
+dbscan_graph.show()
