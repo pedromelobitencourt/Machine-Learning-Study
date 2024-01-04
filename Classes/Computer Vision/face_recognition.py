7b7f251e0a38f0dcb7621a1bcee5cc63e5a59188
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-01-04 13:59:58 -0300

    finish face recognition

--- Classes/Computer Vision/face_recognition.py
+++ Classes/Computer Vision/face_recognition.py
@@ -34,3 +34,17 @@ recognizer.read('LBPH_classifier.yml')
 test_image = '../../Databases/yalefaces/test/subject10.sad.gif'
 image = Image.open(test_image).convert('L')
 np_image = np.array(image, 'uint8')
+
+print(np_image, '\n\n\n')
+
+forecast_id, _ = recognizer.predict(np_image)
+print('forecast id:', forecast_id)
+
+right_id = int(os.path.split(test_image)[1].split('.')[0].replace('subject', ''))
+print('right id:', right_id, '\n')
+
+cv2.putText(np_image, 'P:' + str(forecast_id), (30, 100 + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0))
+cv2.putText(np_image, 'R:' + str(forecast_id), (30, 100 + 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0))
+cv2.imshow('Prediction', np_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
