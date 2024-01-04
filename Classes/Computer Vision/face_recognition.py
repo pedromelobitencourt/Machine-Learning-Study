dad8472f45245d84e89c837492d3279f48543551
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-01-04 09:41:00 -0300

    continue face recognition

--- Classes/Computer Vision/face_recognition.py
+++ Classes/Computer Vision/face_recognition.py
@@ -10,10 +10,10 @@ def image_data():
 
     for path in paths:
         image = Image.open(path).convert('L') # L: one image channel
-        image_np = np.array(image, 'uint8')
+        np_image = np.array(image, 'uint8')
         id = int(os.path.split(path)[1].split('.')[0].replace('subject', ''))
 
-        faces.append(image_np)
+        faces.append(np_image)
         ids.append(id)
 
     return np.array(ids), faces
@@ -23,6 +23,14 @@ ids, faces = image_data()
 print(ids, '===================================================================================================================\n')
 print(faces, '===================================================================================================================\n')
 
-lbph = cv2.face.LBPHFaceRecognizer_create()
-lbph.train(faces, ids)
-lbph.write('LBPH_classifier.yml')
+## Creating a classifier
+# lbph = cv2.face.LBPHFaceRecognizer_create()
+# lbph.train(faces, ids)
+# lbph.write('LBPH_classifier.yml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('LBPH_classifier.yml')
+
+test_image = '../../Databases/yalefaces/test/subject10.sad.gif'
+image = Image.open(test_image).convert('L')
+np_image = np.array(image, 'uint8')
